118228e1c5e17d407fec68434077cc784b3764b1
BoxThatBeat <buitenwerfa@hotmail.ca>
2021-11-05 14:10:17 -0400

    added Spring Boot files

---
+++ project/src/main/java/ca/bookstore3005/project/models/Book.java
@@ -0,0 +1,11 @@
+package ca.bookstore3005.project.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Book {
+
+  private String text;
+}
---
+++ project/src/test/java/ca/bookstore3005/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.bookstore3005.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
