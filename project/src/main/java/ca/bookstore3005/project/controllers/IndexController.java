706805eb3236844378aaf01f9a530a4e897eeb89
BoxThatBeat <buitenwerfa@hotmail.ca>
2021-11-05 14:47:54 -0400

    Thymeleaf example implemented

--- project/src/main/java/ca/bookstore3005/project/controllers/IndexController.java
+++ project/src/main/java/ca/bookstore3005/project/controllers/IndexController.java
@@ -1,15 +1,22 @@
 package ca.bookstore3005.project.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@RestController
+import ca.bookstore3005.project.models.Book;
+
+@Controller
 public class IndexController {
 
-  @RequestMapping("/")
-  public String index() {
+  @RequestMapping(value = "/", method = RequestMethod.GET)
+  public String index(Model model) {
 
-    return "Hello World";
+    Book book = new Book("testText");
+    model.addAttribute("book", book);
+    
+    return "index";
   }
 }
 
--- project/src/main/java/ca/bookstore3005/project/models/Book.java
+++ project/src/main/java/ca/bookstore3005/project/models/Book.java
@@ -1,9 +1,11 @@
 package ca.bookstore3005.project.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class Book {
 
