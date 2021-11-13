918e485967778c68f4b0b2c7d63c6d16d7073671
hadicheaito1 <hadicheaito1@gmail.com>
2021-11-13 18:31:52 -0500

    Putting attributes in models

--- project/src/main/java/ca/bookstore3005/project/models/BankAccount.java
+++ project/src/main/java/ca/bookstore3005/project/models/BankAccount.java
@@ -1,5 +1,21 @@
 package ca.bookstore3005.project.models;
 
+import java.math.BigDecimal;
+
+import org.springframework.data.annotation.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * View data object used to contruct book table.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BankAccount {
-    
+
+    @Id
+    private int bank_number;
+    private BigDecimal amount;
 }
--- project/src/main/java/ca/bookstore3005/project/models/Customer.java
+++ project/src/main/java/ca/bookstore3005/project/models/Customer.java
@@ -1,5 +1,25 @@
 package ca.bookstore3005.project.models;
 
+import java.math.BigDecimal;
+
+import org.springframework.data.annotation.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * View data object used to contruct book table.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
-    
+    @Id
+    private String email;
+    private String password;
+    private String name;      
+    private String phone;
+    private String address_street_num;
+    private String address_street_name;
+    private String address_street_postalString;
 }
