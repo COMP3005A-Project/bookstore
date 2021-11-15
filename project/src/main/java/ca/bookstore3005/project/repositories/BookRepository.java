e4514447435fa47aa3adcbe57bc6c2d42676c781
Nabeel Warsalee <nabeel.warsalee@gmail.com>
2021-11-15 16:07:08 -0500

    Edit ISBN query in BookRepo
    Added two new functions in BookRepo, one for finding books by ISBN and another that does the same thing but for multiple ISBNs

--- project/src/main/java/ca/bookstore3005/project/repositories/BookRepository.java
+++ project/src/main/java/ca/bookstore3005/project/repositories/BookRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ca.bookstore3005.project.models.Book;
 
@@ -11,7 +12,7 @@ import ca.bookstore3005.project.models.Book;
 public interface BookRepository extends CrudRepository<Book, String> {
 
   @Query("SELECT * FROM book WHERE isbn = :isbn")
-  Book findByISBN(String iSBN);
+  Book findByISBN(@Param("isbn") String isbn);
 
   @Query("SELECT * FROM book")
   List<Book> findAllBooks();
