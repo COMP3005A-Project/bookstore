d915a402cebe1e2813c4acc2458638bbf1bb77de
BoxThatBeat <buitenwerfa@hotmail.ca>
2021-11-20 11:33:50 -0500

    Added new book view with a link to it on the books page

--- project/src/main/java/ca/bookstore3005/project/controllers/BookController.java
+++ project/src/main/java/ca/bookstore3005/project/controllers/BookController.java
@@ -14,6 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import ca.bookstore3005.project.models.Book;
 import ca.bookstore3005.project.models.IsbnPacket;
@@ -48,6 +49,14 @@ public class BookController {
     }
     
     return "books";
+  }
+
+  @GetMapping("/book")
+  public String book(@RequestParam String isbn, Model model) {
+
+    model.addAttribute("book", bookService.getBookByISBN(isbn));
+
+    return "book";
   }
 
   /**
